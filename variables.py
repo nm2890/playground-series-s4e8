1dfe56ea50f0b7f96d84af52b369dc90354fe26c
Nico <Nico>
2024-08-01 18:23:07 +0200

    correct dataprocessing + variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+dict_label_target = {'e' : 1,
+                    'p' : 0}
+dict_target_label = {1 : 'e',
+                    0 : 'p'}
+
+TARGET_COL = 'class'
+CAT_COLS = ['cap-shape', 'cap-surface', 'cap-color',
+       'does-bruise-or-bleed', 'gill-attachment', 'gill-spacing', 'gill-color',
+       'stem-root', 'stem-surface', 'stem-color',
+       'veil-type', 'veil-color', 'has-ring', 'ring-type', 'spore-print-color',
+       'habitat', 'season']
+
+CONT_COLS = ['cap-diameter','stem-height','stem-width']
